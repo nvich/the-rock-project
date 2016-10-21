e8c608973d829d83c1955f599b4ea4d5ccbb3451
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-10-21 01:09:08 -0200

    [add] user model

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const User = new mongoose.Schema({
+  email: {
+    type: String,
+    required: '{PATH} is required!'
+  }
+});
+
+// Passport-Local-Mongoose will add a username,
+// hash and salt field to store the username,
+// the hashed password and the salt value
+
+// configure to use email for username field
+User.plugin(passportLocalMongoose, { usernameField: 'email' });
+
+module.exports = mongoose.model('User', User);
