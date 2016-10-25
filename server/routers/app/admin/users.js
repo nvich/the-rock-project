dd48cf5107e942c2f14e0dae89c32cf3092215fe
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-10-25 01:59:21 -0200

    #4 [add] users controller

--- server/routers/app/admin/users.js
+++ server/routers/app/admin/users.js
@@ -1,53 +1,15 @@
-const User = require('../../../models/user');
-// Users
+const usersController = require('../../../controllers/users');
+// UserModels
 module.exports = (router) => {
   // get all users
-  router.get('/admin/users', function(req, res){
-    User.find({}, function(err, users){
-      res.render('admin/users/users', {title: "Usuários", users: users});
-    });
-  });
+  router.get('/admin/users', usersController.showUsers);
 
   // get a single user
-  router.get('/admin/users/:id', function(req, res){
-    User.findById(req.params.id, function(err, user){
-      if (err) {
-        res.send(err);
-      }
-      res.render('admin/users/user', {title: "Usuário", user: user});
-    });
-  });
+  router.get('/admin/users/:id', usersController.showUser);
 
   // update a user
-  router.put('/admin/users/:id', function(req, res){
-    User.findById(req.params.id, function(err, user){
-      if(err) {
-        res.send(err);
-      }
-
-      user.name     = req.body.name;
-      user.email    = req.body.email;
-      user.username = req.body.username;
-      user.password = req.body.password;
-
-      user.save(function(err){
-        if(err) {
-          res.send(err);
-        }
-        res.render('admin/users/user', {title: "Usuário", user: user, message: 'Usuário atualizado'});
-      })
-    });
-  });
+  router.put('/admin/users/:id', usersController.updateUser);
 
   // delete a user
-  router.delete('/admin/users/:id', function(req, res){
-    User.remove({
-      _id: req.params.id
-    }, function(err, user){
-      if(err) {
-        res.send(err);
-      }
-      res.render('admin/users/user', {title: "Usuário", user: user, message: 'Usuário deletado'});
-    })
-  });
+  router.delete('/admin/users/:id', usersController.deleteUser);
 }
